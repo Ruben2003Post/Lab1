561588c8b508db45253da933737a3c0bb7176ad8
Ruben Post <rubenpost23@gmail.com>
2019-10-01 11:44:51 +0200

    title and header added

---
+++ lab1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>lab1</title>
+  </head>
+  <body>
+    <h1>Lab1</h1>
+    <?php
+      
+     ?>
+  </body>
+</html>
